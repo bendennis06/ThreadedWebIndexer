403c655d186a9d70e758bffd3a8015d6594bbdbc
bendennis06 <130309802+bendennis06@users.noreply.github.com>
2025-06-18 16:01:32 -0700

    cleaner syntax, allows option to not enter a url

--- Consumer.java
+++ Consumer.java
@@ -1,8 +1,11 @@
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
+//consumer will parse through data
+
 public class Consumer implements Runnable{
     BlockingQueue<String> queue;
+    String wordToSearch;
 
     @Override
     public void run() {
@@ -10,12 +13,19 @@ public class Consumer implements Runnable{
         try {
             String pageTake = queue.take();
             System.out.println("Test page: " + pageTake.substring(0, 200));
+           if (pageTake.contains(wordToSearch)){
+               System.out.println(wordToSearch + " is present");
+            }else{
+               System.out.println(wordToSearch + " is not present.");
+           }
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public Consumer(BlockingQueue<String> queue){
+    public Consumer(BlockingQueue<String> queue, String wordToSearch){
+
         this.queue = queue;
+        this.wordToSearch = wordToSearch;
     }
 }

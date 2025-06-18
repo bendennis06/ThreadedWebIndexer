c41a80fe28532dc5b05e9e8982f5f43fcc401b8d
bendennis06 <130309802+bendennis06@users.noreply.github.com>
2025-06-18 16:01:32 -0700

    included jsoup in files

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -6,14 +7,46 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class Main {
     public static void main(String[] args) {
     BlockingQueue<String> queue = new LinkedBlockingQueue<>(3);
-    Producer producer = new Producer("https://www.fmscout.com/a-football-manager-2024-wonderkids.html", queue);
-    Consumer consumer = new Consumer(queue);
 
-    Thread producerThread = new Thread(producer);
-    Thread consumerThread = new Thread(consumer);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the website URL for the first website: ");
+        String userInputURLOne = scanner.nextLine();
 
-    producerThread.start();
-    consumerThread.start();
+        System.out.println("Enter the website URL for the second website: ");
+        String userInputURLTwo = scanner.nextLine();
+
+        System.out.println("Enter the website URL for the third website: ");
+        String userInputURLThree = scanner.nextLine();
+
+        System.out.println("Enter a word to search for: ");
+        String wordToSearch = scanner.nextLine();
+
+        Producer producerOne = new Producer(userInputURLOne, queue);
+        Consumer consumerOne = new Consumer(queue, wordToSearch);
+
+        Producer producerTwo = new Producer(userInputURLTwo, queue);
+        Consumer consumerTwo = new Consumer(queue, wordToSearch);
+
+        Producer producerThree = new Producer(userInputURLThree, queue);
+        Consumer consumerThree = new Consumer(queue, wordToSearch);
+
+    Thread producerThreadOne = new Thread(producerOne);
+    Thread consumerThreadOne = new Thread(consumerOne);
+
+    Thread producerThreadTwo = new Thread(producerTwo);
+    Thread consumerThreadTwo = new Thread(consumerTwo);
+
+    Thread producerThreadThree = new Thread(producerThree);
+    Thread consumerThreadThree = new Thread(consumerThree);
+
+    producerThreadOne.start();
+    producerThreadTwo.start();
+    producerThreadThree.start();
+
+    consumerThreadOne.start();
+    consumerThreadTwo.start();
+    consumerThreadThree.start();
+
     
         }
     }
